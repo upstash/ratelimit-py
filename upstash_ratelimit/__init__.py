aa6dcc37f6652acb375d8ccf789c05fd1550307a
burak-upstash <burak@upstash.com>
2023-07-17 13:56:26 +0300

    bump version to 0.5.1

--- upstash_ratelimit/__init__.py
+++ upstash_ratelimit/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 from upstash_ratelimit.limiter import FixedWindow, Response, SlidingWindow, TokenBucket
 from upstash_ratelimit.ratelimit import Ratelimit
